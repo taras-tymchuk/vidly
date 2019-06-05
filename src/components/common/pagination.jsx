a9e8290cd1648101feb78c98ea67cba45ff7833c
taras-tymchuk <tymakruk@gmail.com>
2019-06-06 01:25:42 +0300

    Uploaded working copy of project

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
+import React from "react";
+import PropTypes from "prop-types";
+import _ from "lodash";
 
-const Pagination = props => {
-  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+const Pagination = ({ itemsCount, pageSize, currentPage, onPageChange }) => {
   const pagesCount = Math.ceil(itemsCount / pageSize);
-
-  if (pagesCount === 1) {
-    return null;
-  }
-
+  if (pagesCount === 1) return null;
   const pages = _.range(1, pagesCount + 1);
 
   return (
     <nav>
-      <ul className='pagination'>
+      <ul className="pagination">
         {pages.map(page => (
           <li
             key={page}
-            className={page === currentPage ? 'page-item active' : 'page-item'}
+            className={page === currentPage ? "page-item active" : "page-item"}
           >
-            <a className='page-link' onClick={() => onPageChange(page)}>
+            <a className="page-link" onClick={() => onPageChange(page)}>
               {page}
             </a>
           </li>
