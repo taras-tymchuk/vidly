869d35614e17269440bf6e863b3de90b3d08282a
Taras Tymchuk <32062140+taras-tymchuk@users.noreply.github.com>
2019-06-06 21:32:32 +0300

    Created minimal application
    * Added services and components
    
    * Added Like component
    
    * Added pagination
    
    * Added highlighting of pagination
    
    * Implemented pagination
    
    * Added type checking for input properties
    
    * Uploaded working copy of project
    
    * Added routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, Redirect, Switch } from 'react-router-dom';
+import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navbar';
+import MovieForm from './components/movieForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className='container'>
+          <Switch>
+            <Route path='/movies/:id' component={MovieForm} />
+            <Route path='/movies' component={Movies} />
+            <Route path='/customers' component={Customers} />
+            <Route path='/rentals' component={Rentals} />
+            <Route path='/not-found' component={NotFound} />
+            <Redirect from='/' exact to='/movies' />
+            <Redirect to='/not-found' />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
